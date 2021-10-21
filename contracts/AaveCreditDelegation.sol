5b5ad5ede6884f8f0b94c91caa80d106a486a20f
PierreLvl <pierre.louvel@protonmail.com>
2021-10-21 17:48:40 +0200

    optimize state variable / fix some compilation issues

--- contracts/AaveCreditDelegation.sol
+++ contracts/AaveCreditDelegation.sol
@@ -13,16 +13,19 @@ contract AaveCreditDelegation {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    // Address of AAVE Data Provider contract
-    address private AAVE_DATA_PROVIDER;
+    // Interface to AAVE Data Provider contract
+    IProtocolDataProvider private dataProvider;
     
-    // Address of AAVE Lending Pool contract
-    address private AAVE_LENDING_POOL;
+    // Interface to AAVE Lending Pool contract
+    ILendingPool private lendingPool;
+
+    // Amount delegated by the user to AAVE Credit Delegation contract
+    uint256 private amountDelegated = 0;
 
 
     constructor(address _dataProvider, address _lendingPool) {
-        AAVE_DATA_PROVIDER = _dataProvider;
-        AAVE_LENDING_POOL = _lendingPool;
+        dataProvider = IProtocolDataProvider(_dataProvider);
+        lendingPool = ILendingPool(_lendingPool);
     }
 
     function drawCredit(address _asset, uint256 _interestRateMode) external {
@@ -30,34 +33,33 @@ contract AaveCreditDelegation {
         require(_interestRateMode == 1 || _interestRateMode == 2, "Incorrect Interest Rate Mode");
 
         // Get the address of debt tokens
-        (, address sDebtTokenAddr, address vDebtTokenAddr) = IProtocolDataProvider(AAVE_DATA_PROVIDER).getReserveTokensAddresses(_asset);
-        // Assign the address of debtAsset based on the interest rate mode (1 = Stable / 2 = Variable)
-        IERC20 debtAsset = _interestRateMode == 1 ? IStableDebtToken(sDebtTokenAddr) : IVariableDebtToken(vDebtTokenAddr);  
+        (, address sDebtTokenAddr, address vDebtTokenAddr) = dataProvider.getReserveTokensAddresses(_asset);
 
         // Assign and check the amount approved for delegation
-        uint256 amount = debtAsset.borrowAllowance(msg.sender, address(this));
+        uint256 amount;
+        if(_interestRateMode == 1) {
+            amount = IStableDebtToken(sDebtTokenAddr).borrowAllowance(msg.sender, address(this));
+        } else {
+            amount = IVariableDebtToken(vDebtTokenAddr).borrowAllowance(msg.sender, address(this));
+        }
         require(amount > 0, "Amount approved for delegation is 0");
 
         // Borrow amount of asset on behalf of msg.sender
-        ILendingPool(AAVE_LENDING_POOL).borrow(_asset, amount, _interestRateMode, 0, msg.sender);
+        lendingPool.borrow(_asset, amount, _interestRateMode, 0, msg.sender);
 
         // Here, deposit to Beefy 
     }
 
-    function requestDebtRepayment(_asset, _interestRateMode) external {
+    function requestDebtRepayment(address _asset, uint256 _interestRateMode) external {
         // Check correctness of interest rate mode parameter
         require(_interestRateMode == 1 || _interestRateMode == 2, "Incorrect Interest Rate Mode");
 
-        // Get the address of debt tokens
-        (, address sDebtTokenAddr, address vDebtTokenAddr) = IProtocolDataProvider(AAVE_DATA_PROVIDER).getReserveTokensAddresses(_asset);
-        // Assign the address of debtAsset based on the interest rate mode (1 = Stable / 2 = Variable)
-        IERC20 debtAsset = _interestRateMode == 1 ? IStableDebtToken(sDebtTokenAddr) : IVariableDebtToken(vDebtTokenAddr);
-
         // Here, withdraw from Beefy
+        uint256 amount = IERC20(_asset).balanceOf(address(this));
 
-        debtAsset.approve(AAVE_LENDING_POOL, amount);
+        IERC20(_asset).approve(address(lendingPool), amount);
         // Repay the amount of asset on behalf of msg.sender
-        ILendingPool(AAVE_LENDING_POOL).repay(_asset, amount, _interestRateMode, msg.sender);
+        lendingPool.repay(_asset, amount, _interestRateMode, msg.sender);
 
     }
 }
